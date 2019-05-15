6081d4dfb25aa95b8f51899a50ae4fe9f85bfeae
terui <ate2934rui@gmail.com>
2019-05-15 09:30:41 +0900

    [add]全ファイルの挿入

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour {
+
+	public void TestCall () {
+		Debug.Log ("Heloo Unity");
+	}
+}
---
+++ Assets/HiyokoGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiyokoGenerator : MonoBehaviour {
+
+	public GameObject obj;
+	public float interval = 3.0f;
+
+	// Use this for initialization
+		void Start () {
+		InvokeRepeating ("SpawnObj", 0.1f, interval); 	
+	}
+	
+	// Update is called once per frame
+	void SpawnObj () {
+		Instantiate (obj, transform.position,transform.rotation);
+	}
+}
